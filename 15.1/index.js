35b71849fa8267c4540036140209181257399eb8
aakashloyar <aakashloyar@gmail.com>
2025-02-09 18:11:40 +0530

    done some network porting

--- 15.1/index.js
+++ 15.1/index.js
@@ -1,6 +1,15 @@
 const express=require('express')
+const mongoose=require('mongoose')
 const app=express();
+const mongouri="mongodb://mongo:27017/mydatabase"
 app.get('/',(req,res)=>{
     res.send('Hello world is mine');
 })
+console.log(mongouri)
+try{
+    mongoose.connect(mongouri);
+    console.log('connected to mongo successfully')
+} catch {
+    console.log('error while connecting to mongo')
+}
 app.listen(3000);
